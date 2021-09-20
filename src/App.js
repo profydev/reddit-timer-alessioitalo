ab757687655e22886a681dccf6b72b9621099d09
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2021-09-20 18:11:30 +0100

    built skeleton add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import MarketingPage from './pages/MarketingPage';
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <Route path="/" exact>
+        <MarketingPage />
+      </Route>
+      <Route path="/search">
+        <SearchPage />
+      </Route>
+    </>
   );
 }
 
