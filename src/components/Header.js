81f38d9d032236b331ae6e29f82edabab1078924
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2021-09-21 08:47:55 +0100

    style navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+import Logo from '../logo.svg';
 
-const Header = () => <header>this is the header</header>;
+const StyledHeader = styled.header`
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 40px 80px;
+
+& ul{
+    display: flex;
+    justify-content: space-around;
+}
+
+& ul a{
+    text-decoration: none;
+    color: inherit;
+    list-style: none;
+    padding: 0 25px;
+}
+`;
+
+const Header = () => (
+  <StyledHeader>
+    <img src={Logo} alt="logo" />
+    <ul>
+      <a href="/">
+        <li>Search</li>
+      </a>
+      <a href="/">
+        <li>How it works</li>
+      </a>
+
+      <a href="/">
+        <li>About</li>
+      </a>
+    </ul>
+  </StyledHeader>
+);
 
 export default Header;
