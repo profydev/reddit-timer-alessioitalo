6aa6ea2bdc688bb7523a77644c9e35411b08a78c
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2021-09-21 08:22:02 +0100

    add browserRouter to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
 import MarketingPage from './pages/MarketingPage';
 import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Route path="/" exact>
         <MarketingPage />
       </Route>
       <Route path="/search">
         <SearchPage />
       </Route>
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'normalize.css';
 import './index.css';
 
 // eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
