a1b9c9bd93c3771afa4d879ef51308d8b1b02721
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2021-09-21 08:36:05 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 import MarketingPage from './pages/MarketingPage';
 import SearchPage from './pages/SearchPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Route path="/" exact>
         <MarketingPage />
       </Route>
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = () => <header>this is the header</header>;
+
+export default Header;
